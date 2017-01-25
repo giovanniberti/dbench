cd9821820f0c1eacf14c8c141d0adca398baca52
Giovanni Berti <dev.giovanniberti@gmail.com>
2017-01-25 16:54:09 +0100

    refactor(connection): add IntoConnectionParams, ConnectionError and ParamsError
    Signed-off-by: Giovanni Berti <dev.giovanniberti@gmail.com>

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -23,4 +23,63 @@ pub struct ConnectionData<'c> {
     pub password: Option<Cow<'c, str>>,
 }
 
+#[derive(Debug)]
+pub enum ParamsError {
+    UnsupportedBackendError,
+    MalformedURLError
+}
+
+impl Display for ParamsError {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl Error for ParamsError {
+    fn description(&self) -> &str {
+        match *self {
+            ParamsError::UnsupportedBackendError => "Unsupported backend!",
+            ParamsError::MalformedURLError => "Error while trying to parse URL!"
+        }
+    }
+}
+
+pub trait IntoConnectionParams<'c> {
+    fn into(self) -> Result<ConnectionParams<'c, Database>, ParamsError>;
+}
+
+impl<'c> IntoConnectionParams<'c> for ConnectionParams<'c, Database> {
+    fn into(self) -> Result<ConnectionParams<'c, Database>, ParamsError> {
+        Ok(self)
+    }
+}
+
 pub struct Connection {}
+
+#[derive(Debug)]
+pub enum ConnectionError {
+    ParamsError(ParamsError),
+    BackendError(BackendError)
+}
+
+impl Display for ConnectionError {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl Error for ConnectionError {
+    fn description(&self) -> &str {
+        match *self {
+            ConnectionError::ParamsError(_) => "Wrong parameters!",
+            ConnectionError::BackendError(_) => "Backend error! {}"
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match *self {
+            ConnectionError::ParamsError(ref e) => Some(*&e),
+            ConnectionError::BackendError(ref e) => Some(*&e)
+        }
+    }
+}
