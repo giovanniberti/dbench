315f9777844646d2e7279795529963ee4a16b725
Giovanni Berti <dev.giovanniberti@gmail.com>
2017-01-25 16:58:36 +0100

    refactor(channel) implement DbChannel for MySQl and Postgres
    This commit implements the query operation for MySQL and Postgres. The refactor is now complete! :)

--- src/db/channel.rs
+++ src/db/channel.rs
@@ -1,5 +1,25 @@
 use std::error::Error;
+use mysql::Pool;
+use postgres::Connection;
 
 pub trait DbChannel {
     fn query(&self, &str) -> Result<(), String>;
+}
+
+impl DbChannel for Pool {
+    fn query(&self, query: &str) -> Result<(), String> {
+        match self.prep_exec(query, ()) {
+            Ok(_) => Ok(()),
+            Err(err) => Err(err.description().to_string())
+        }
+    }
+}
+
+impl DbChannel for Connection {
+    fn query(&self, query: &str) -> Result<(), String> {
+        match self.execute(query, &[]) {
+            Ok(_) => Ok(()),
+            Err(err) => Err(err.description().to_string())
+        }
+    }
 }
